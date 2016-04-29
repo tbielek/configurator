9672908a2b702679e23ce2b6efd51c9d159e5ce3
weevil <argonaut.linux@gmail.com>
2016-04-28 20:40:13 -0400

    Add installation script for bash.

--- features/common/bash/install.sh
+++ features/common/bash/install.sh
@@ -2,3 +2,10 @@
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
 cd "$SCRIPT_DIR"
+
+install_path="../../../repos/bash"
+
+git clone git@bitbucket.org:boweevil/bash.git "${install_path}"
+cd "${install_path}"
+git submodule update --init --recursive
+./install.sh
