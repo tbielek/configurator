83086b4862b7f0bbc2d08f2a5f9ba45e7fcaedca
weevil <argonaut.linux@gmail.com>
2016-04-28 20:56:10 -0400

    Add install script for pkgs_cli.

--- features/common/pkgs_cli/install.sh
+++ features/common/pkgs_cli/install.sh
@@ -2,3 +2,10 @@
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
 cd "$SCRIPT_DIR"
+
+install_path="../../../repos/pkgs_cli"
+
+git clone git@bitbucket.org:boweevil/pkgs_cli.git "${install_path}"
+cd "${install_path}"
+git submodule update --init --recursive
+./install.sh
