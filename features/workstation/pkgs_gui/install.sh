a64beb90e1d2e0a4279f7fbca7512c310ea036a9
boweevil <argonaut.linux@gmail.com>
2016-06-09 20:24:24 -0400

    Add install script for pkgs_gui

--- features/workstation/pkgs_gui/install.sh
+++ features/workstation/pkgs_gui/install.sh
@@ -2,3 +2,10 @@
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
 cd "$SCRIPT_DIR"
+
+install_path="../../../repos/pkgs_gui"
+
+git clone git@bitbucket.org:boweevil/pkgs_gui.git "${install_path}"
+cd "${install_path}"
+git submodule update --init --recursive
+./install.sh
