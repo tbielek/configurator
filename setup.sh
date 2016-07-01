3d2bf81f5bc8291ad5677b1db84bc20234ebba85
boweevil <argonaut.linux@gmail.com>
2016-06-30 21:15:52 -0400

    Add update functionality to setup.sh.

--- setup.sh
+++ setup.sh
@@ -24,13 +24,31 @@ features_workstation(){
   unset feature
 }
 
+pull_updates(){
+  git pull
+  git submodule update --init --recursive
+  cd "${SCRIPT_DIR}/repos"
+  for i in $(ls); do
+    echo ----------------------------------------------------------------------
+    echo "Updating ${i}..."
+    cd "${i}"
+    git pull
+    git submodule update --init --recursive
+    echo "Done updating ${i}"
+    cd -
+  done
+  cd "${SCRIPT_DIR}"
+}
+
 usage(){
   echo ""
-  echo "Usage: $0 [[-c] | [-w] | [-h]]"
+  echo "Usage: $0 [[-c] | [-w] | [-u] | [-h]]"
   echo ""
   echo "  -c, --common        Install common features."
   echo ""
   echo "  -w, --workstation   Install common and workstation features."
+  echo ""
+  echo "  -u, --update        Update all repositories."
   echo ""
   echo "  -h, --help          Print this help."
   echo ""
@@ -58,6 +76,10 @@ else
       -w | --workstation )
         features_common
         features_workstation
+        exit 0
+        ;;
+      -u | --update )
+        pull_updates
         exit 0
         ;;
       -h | --help )
