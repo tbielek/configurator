5d1e6cb473bf6baf03fddac2a692b416ae877552
boweevil <argonaut.linux@gmail.com>
2016-07-13 20:06:47 -0400

    Switch repos to public URLs.

--- features/common/pkgs_cli/install.sh
+++ features/common/pkgs_cli/install.sh
@@ -3,9 +3,10 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
 cd "$SCRIPT_DIR"
 
+repo_url="https://boweevil@bitbucket.org/boweevil/pkgs_cli.git"
 install_path="../../../repos/pkgs_cli"
 
-git clone git@bitbucket.org:boweevil/pkgs_cli.git "${install_path}"
+git clone "${repo_url}" "${install_path}"
 cd "${install_path}"
 git submodule update --init --recursive
 ./install.sh
