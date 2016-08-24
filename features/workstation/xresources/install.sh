f26055ee8acf61cd660cc1bda7dc2938033182f7
boweevil <argonaut.linux@gmail.com>
2016-08-24 11:24:07 -0400

    Add xresources feature.

---
+++ features/workstation/xresources/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
+cd "$SCRIPT_DIR"
+
+repo_url="https://boweevil@bitbucket.org/boweevil/xresources.git"
+install_path="../../../repos/xresources"
+
+git clone "${repo_url}" "${install_path}"
+cd "${install_path}"
+git submodule update --init --recursive
+./install.sh
